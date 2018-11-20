d9980849bb383ee0834cc398d2510919be2cf8b5
seibed <david.seibert@emc.com>
2018-11-20 16:15:59 -0500

    Updating version.

--- src/main/resources/static/javascript/S3Browser.js
+++ src/main/resources/static/javascript/S3Browser.js
@@ -61,7 +61,7 @@ S3Browser = function( options, $parent ) {
 
 // release version
 /** @define {string} */
-var S3_BROWSER_VERSION = '0.9.1';
+var S3_BROWSER_VERSION = '1.0.0';
 S3Browser.version = S3_BROWSER_VERSION;
 
 /** @define {boolean} */
